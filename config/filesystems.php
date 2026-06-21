97f3a0c9e11b02a419f711be2425883bbc9029b3
Andrew Hargrave <me@andyh.app>
2026-06-21 12:28:33 +0100

    filesystem config

--- config/filesystems.php
+++ config/filesystems.php
@@ -4,29 +4,35 @@ declare(strict_types=1);
 
 return [
 
-    'default' => env('FILESYSTEM_DISK', 'public'),
-
     'disks' => [
+        'local' => [
+            'driver' => 'local',
+            'root'   => storage_path('app'),
+        ],
+
         'public' => [
             'driver'                  => 's3',
-            'key'                     => env('R2_ACCESS_KEY_ID'),
-            'secret'                  => env('R2_SECRET_ACCESS_KEY'),
-            'region'                  => 'auto',
-            'bucket'                  => env('R2_PUBLIC_BUCKET'),
-            'url'                     => env('R2_PUBLIC_URL'),
-            'endpoint'                => env('R2_ENDPOINT'),
-            'use_path_style_endpoint' => false,
+            'key'                     => env('AWS_ACCESS_KEY_ID'),
+            'secret'                  => env('AWS_SECRET_ACCESS_KEY'),
+            'region'                  => env('AWS_DEFAULT_REGION', 'auto'),
+            'bucket'                  => env('AWS_BUCKET'),
+            'url'                     => env('AWS_URL'),
+            'endpoint'                => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'cdn_endpoint'            => env('AWS_CDN_ENDPOINT'),
+            'visibility'              => 'public',
             'throw'                   => false,
         ],
 
         'private' => [
             'driver'                  => 's3',
-            'key'                     => env('R2_ACCESS_KEY_ID'),
-            'secret'                  => env('R2_SECRET_ACCESS_KEY'),
-            'region'                  => 'auto',
-            'bucket'                  => env('R2_PRIVATE_BUCKET'),
-            'endpoint'                => env('R2_ENDPOINT'),
-            'use_path_style_endpoint' => false,
+            'key'                     => env('AWS_PRIVATE_ACCESS_KEY_ID', env('AWS_ACCESS_KEY_ID')),
+            'secret'                  => env('AWS_PRIVATE_SECRET_ACCESS_KEY', env('AWS_SECRET_ACCESS_KEY')),
+            'region'                  => env('AWS_PRIVATE_DEFAULT_REGION', env('AWS_DEFAULT_REGION', 'auto')),
+            'bucket'                  => env('AWS_PRIVATE_BUCKET'),
+            'url'                     => env('AWS_PRIVATE_URL'),
+            'endpoint'                => env('AWS_PRIVATE_ENDPOINT', env('AWS_ENDPOINT')),
+            'use_path_style_endpoint' => env('AWS_PRIVATE_USE_PATH_STYLE_ENDPOINT', env('AWS_USE_PATH_STYLE_ENDPOINT', false)),
             'throw'                   => false,
         ],
     ],
